2f9037898c07e26943be516494942eb48f7880e3
Valeron <valerontoscano@gmail.com>
2023-03-29 18:01:44 +0530

    Results base UI

--- lib/screens/results.dart
+++ lib/screens/results.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+
+class ResultsController extends GetxController {
+  var companiesFound = ['test'].obs;
+  final TextEditingController searchController = TextEditingController();
+}
 
 class Results extends StatelessWidget {
   var box = GetStorage();
+  ResultsController controller = Get.put(ResultsController());
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +18,42 @@ class Results extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: Container(child: Center(child: Text(box.read('response')['data']['companiesList'].toString()),)),
+      body: Obx(() => Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "Companies List",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                ),
+                Text("${controller.companiesFound.length} companies Found"),
+                TextFormField(
+                  controller: controller.searchController,
+                  decoration: const InputDecoration(
+                    isDense: true,
+                    hintText: "Search Companies",
+                    hintStyle: TextStyle(fontWeight: FontWeight.bold),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(width: 1, color: Colors.black),
+                        borderRadius: BorderRadius.zero),
+                    contentPadding: EdgeInsets.fromLTRB(10, 7, 0, 7),
+                  ),
+                ),
+                // ListView.builder(
+                //     itemBuilder: (BuildContext, index) {
+                //       return ListTile(
+                //         title: Text("helo"),
+                //       );
+                //     },
+                //     itemCount: controller.companiesFound.length)
+              ],
+            ),
+          )),
     );
   }
 }
